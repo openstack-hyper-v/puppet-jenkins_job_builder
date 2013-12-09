3a95e75fd07d5728f6350fa9ed571ef3dd852e80
Tim Rogers <tiroger@microsoft.com>
2013-12-09 15:58:57 -0500

    added config file generation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,10 @@
 class jenkins_job_builder (
   $jjb_checkout_dir = $jenkins_job_builder::params::jjb_checkout_dir,
   $jjb_source_repo  = $jenkins_job_builder::params::jjb_source_repo,
+  $jjb_username     = $jenkins_job_builder::params::jjb_username,
+  $jjb_token        = $jenkins_job_builder::params::jjb_token,
+  $jjb_jenkins_url  = $jenkins_job_builder::params::jjb_jenkins_url,
+  $jjb_configfile   = $jenkins_job_builder::params::jjb_configfile,
 ) inherits jenkins_job_builder::params {
 
   package { "git": ensure => present }
@@ -15,10 +19,16 @@ class jenkins_job_builder (
   }
   
   exec { 'setup_jenkins_job_builder':
-    command     => "python setup.py install",
-    cwd         => $jjb_checkout_dir,
-    path        => "/usr/bin", 
-    require     => [Class['python'], Vcsrepo[$jjb_checkout_dir]],
+    command => "python setup.py install",
+    cwd     => $jjb_checkout_dir,
+    path    => "/usr/bin", 
+    require => [Class['python'], Vcsrepo[$jjb_checkout_dir]],
+    creates => '/usr/local/bin/jenkins-jobs',
+  }
+
+  file { $jjb_configfile:
+    ensure  => file,
+    content => template('configfile.erb'),
   }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,13 @@
 #
 class jenkins_job_builder::params {
 
-  $jjb_checkout_dir       = '/opt/src/jenkins_job_builder'
-  $jjb_source_repo        = 'git://github.com/openstack-infra/jenkins-job-builder.git'
+  $jjb_checkout_dir = '/opt/src/jenkins_job_builder'
+  $jjb_source_repo  = 'git://github.com/openstack-infra/jenkins-job-builder.git'
+  
+  $jjb_username     = 'JenkinsJobBuilder'
+  $jjb_token        = ''
+  $jjb_jenkins_url  = 'http://localhost:8080/'
+
+  $jjb_configfile   = '/etc/jenkins_jobs/jenkins_jobs.ini'
 
 } # Class:: jenkins_job_builder::params
