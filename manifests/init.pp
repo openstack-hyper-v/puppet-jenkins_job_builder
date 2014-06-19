247e02e1b5329141e5c90a842abe4bf9a7875b69
Tim Rogers <tiroger@microsoft.com>
2014-06-19 10:26:25 -0700

    now appears to install properly

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,8 +7,19 @@ class jenkins_job_builder (
   $jjb_configfilepath = $jenkins_job_builder::params::jjb_configfilepath,
 ) inherits jenkins_job_builder::params {
 
-  package { "git": ensure => present }
-  class { 'python': version => 'system', }
+  if !defined(Package['git']){
+    package { 'git': ensure => present }
+  }
+  if !defined(Package['python-setuptools']){
+    package { 'python-setuptools':
+      ensure  => present,
+      require => Class['python'],
+    }
+  }
+  class { 'python':
+    version => 'system',
+    pip     => true,
+  }
 
   vcsrepo { $jjb_checkout_dir:
     ensure   => latest,
@@ -17,19 +28,25 @@ class jenkins_job_builder (
     revision => "master",
     require  => Package['git'],
   }
-  
+
   exec { 'setup_jenkins_job_builder':
     command => "python setup.py install",
     cwd     => $jjb_checkout_dir,
-#    path    => "/usr/bin", 
-    require => [Class['python'], Vcsrepo[$jjb_checkout_dir]],
-#    creates => '/usr/local/bin/jenkins-jobs',
+    path    => $::osfamily ? {
+                'windows' => 'c:/windows/system32;c:/windows;c:/python27/bin',
+                default   => '/usr/bin',
+               },
+    require => [Package['python-setuptools'], Vcsrepo[$jjb_checkout_dir]],
+    creates => $::osfamily ? {
+                'windows' => '',
+                default   => '/usr/local/bin/jenkins-jobs',
+               },
   }
 
   file { $jjb_configfilepath:
     ensure => directory,
   }
-  
+
   file { "${jjb_configfilepath}/jenkins_jobs.ini":
     ensure  => file,
     content => template('jenkins_job_builder/configfile.erb'),
@@ -37,4 +54,3 @@ class jenkins_job_builder (
   }
 
 }
-
