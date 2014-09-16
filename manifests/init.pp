e9dc0d45a8231e655f4686d3959bb051442823e2
Tim Rogers <tiroger@microsoft.com>
2014-09-16 16:12:00 -0700

    updating to allow install direct from repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class jenkins_job_builder (
-#  $jjb_checkout_dir   = $jenkins_job_builder::params::jjb_checkout_dir,
-#  $jjb_source_repo    = $jenkins_job_builder::params::jjb_source_repo,
+  $jjb_src_install    = $jenkins_job_builder::params::jjb_src_install
+  $jjb_checkout_dir   = $jenkins_job_builder::params::jjb_checkout_dir,
+  $jjb_source_repo    = $jenkins_job_builder::params::jjb_source_repo,
   $jjb_username       = $jenkins_job_builder::params::jjb_username,
   $jjb_token          = $jenkins_job_builder::params::jjb_token,
   $jjb_jenkins_url    = $jenkins_job_builder::params::jjb_jenkins_url,
@@ -16,8 +17,11 @@ class jenkins_job_builder (
 #      require => Class['python'],
 #    }
 #  }
-
-  $packs = ['jenkins-job-builder', 'PyYAML', 'argparse']
+  $base_packs = ['PyYAML', 'argparse']
+  $packs = str2bool($jjb_src_install) ? {
+    true  => concat($base_packs, 'python-setuptools'),
+    false => concat($base_packs, 'jenkins-job-builder'),
+  }
 
   ensure_resource('class', 'python', {'pip' => true })
 #  class { 'python':
@@ -31,27 +35,29 @@ class jenkins_job_builder (
     require  => Class['python'],
   }
 
-#  vcsrepo { $jjb_checkout_dir:
-#    ensure   => latest,
-#    provider => git,
-#    source   => $jjb_source_repo,
-#    revision => "master",
-#    require  => Package['git'],
-#  }
-
-#  exec { 'setup_jenkins_job_builder':
-#    command => "python setup.py install",
-#    cwd     => $jjb_checkout_dir,
-#    path    => $::osfamily ? {
-#                'windows' => 'c:/windows/system32;c:/windows;c:/python27/bin',
-#                default   => '/usr/bin',
-#               },
-#    require => [Package['python-setuptools'], Vcsrepo[$jjb_checkout_dir]],
-#    creates => $::osfamily ? {
-#                'windows' => '',
-#                default   => '/usr/local/bin/jenkins-jobs',
-#               },
-#  }
+  if str2bool($jjb_src_install) {
+    vcsrepo { $jjb_checkout_dir:
+      ensure   => latest,
+      provider => git,
+      source   => $jjb_source_repo,
+      revision => "master",
+#      require  => Package['git'],
+    }
+   
+    exec { 'install_jenkins_job_builder':
+      command => "python setup.py install",
+      cwd     => $jjb_checkout_dir,
+      path    => $::osfamily ? {
+                  'windows' => 'c:/windows/system32;c:/windows;c:/python27/bin',
+                  default   => '/usr/bin',
+                 },
+      require => [Package['python-setuptools'], Vcsrepo[$jjb_checkout_dir]],
+      creates => $::osfamily ? {
+                  'windows' => '',
+                  default   => '/usr/local/bin/jenkins-jobs',
+                 },
+    }
+  }
 
   file { $jjb_configfilepath:
     ensure => directory,
